ef68a2cb6646739982459f2721fe252dc5f740e0
Attogram Project <attogram@users.noreply.github.com>
2017-11-12 21:45:49 +0100

    base ref

--- src/Base.php
+++ src/Base.php
@@ -2,24 +2,62 @@
 
 namespace Attogram\SharedMedia\Gallery;
 
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
 class Base
 {
-    const VERSION = '0.0.1';
+    const VERSION = '0.0.2';
 
     protected $twig;
+    protected $uriBase;
+    protected $uriRelative;
 
     public function __construct()
     {
-        $this->twig = new \Twig_Environment(
-            new \Twig_Loader_Filesystem('../views/'),
+        $this->twig = new Twig_Environment(
+            new Twig_Loader_Filesystem('../views/'),
             [
                 'cache' => '../cache/',
                 'auto_reload' => true,
             ]
         );
+
+        if (!$this->setUri()) {
+            return false;
+        }
         $this->route();
     }
-    
+
+    private function setUri()
+    {
+        $this->uriBase = strtr($this->getServer('SCRIPT_NAME'), ['index.php' => '']);
+        $this->uriRelative = strtr($this->getServer('REQUEST_URI'), [$this->uriBase => '/']);
+        if (preg_match('#/$#', $this->uriRelative)) { // If has slash at end
+            return true;
+        }
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: '.rtrim($this->uriBase, '/').$this->uriRelative.'/');  // Force trailing slash
+        return false;
+    }
+
+    private function getServer($name)
+    {
+        if (isset($_SERVER[$name])) {
+            return $_SERVER[$name];
+        }
+    }
+
+    protected function getDefaultData()
+    {
+        return [
+            'title' => 'Shared Media Gallery',
+            'version' => self::VERSION,
+            'uriBase' => $this->uriBase,
+            'uriRelative' => $this->uriRelative,
+        ];
+    }
+
     protected function route()
     {
     }
--- src/Gallery.php
+++ src/Gallery.php
@@ -4,21 +4,25 @@ namespace Attogram\SharedMedia\Gallery;
 
 class Gallery extends Base
 {
-    const VERSION = '0.0.1';
-
-    protected $twig;
+    const VERSION = '0.0.2';
 
     protected function route()
     {
-        $this->homePage();
-    }
-
-    private function homePage()
-    {
-        $data = [
-            'title' => 'Shared Media Gallery',
-            'version' => self::VERSION,
-        ];
-        $this->twig->display('home.twig', $data);
+        $uri = explode('/', $this->uriRelative);
+        switch ($uri[1]) {
+            case '':
+                $this->twig->display('home.twig', $this->getDefaultData());
+                break;
+            case 'about':
+                $this->twig->display('about.twig', $this->getDefaultData());
+                break;
+            case 'admin':
+                $this->twig->display('admin.twig', $this->getDefaultData());
+                break;
+            default:
+                header('HTTP/1.0 404 Not Found');
+                $this->twig->display('error.twig', $this->getDefaultData());
+                break;
+        }
     }
 }
