70119bd1de9273915aa5bb4ea74cd2e7caad07d9
Attogram Project <attogram@users.noreply.github.com>
2017-12-27 11:25:24 +0100

    accessControl 403's

--- src/TraitAccessControl.php
+++ src/TraitAccessControl.php
@@ -3,39 +3,56 @@
 namespace Attogram\SharedMedia\Gallery;
 
 use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
 
 trait TraitAccessControl
 {
     /**
-     * Allow access by IP address, or exit with error 403;
+     * Allow access by IP address, or exit with error 403
      * @return void
      */
     private function accessControl()
     {
         $allowedIps = $this->getAllowedIps();
-        if (!$allowedIps) {
-            $this->error403('Access Control Error');
-        }
-        $remoteIp = $this->getServer('REMOTE_ADDR');
+        $remoteIp = $this->getRemoteIp();
         if (!in_array($remoteIp, $allowedIps)) {
             $this->error403('Access Denied');
         }
     }
 
     /**
-     * @return array|void
+     * Get list of Allowed IP Addresses from the .env file, or exit with error 403
+     * @return array
      */
     private function getAllowedIps()
     {
-        $dotenv = new Dotenv(__DIR__ . '/../');
-        $dotenv->load();
-        $envAllowedIps = getenv('ALLOWED_IPS');
-        if ($envAllowedIps) {
-            return explode(',', $envAllowedIps);
+        try {
+            $dotenv = new Dotenv(__DIR__ . '/../');
+            $dotenv->load();
+        } catch (InvalidPathException $error) {
+            $this->error403('Env Not Found');
         }
+        $envAllowedIps = getenv('ALLOWED_IPS');
+        if (!$envAllowedIps) {
+            $this->error403('Allow List Not Found');
+        }
+        return explode(',', $envAllowedIps);
     }
 
     /**
+     * Get Remote IP Address, or exit with error 403
+     * @return string
+     */
+    private function getRemoteIp()
+    {
+        $remoteIp = $this->getServer('REMOTE_ADDR');
+        if (!$remoteIp) {
+            $this->error403('Remote IP Not Found');
+        }
+        return $remoteIp;
+    }
+    /**
+     * Send 403 Error with message, then exit
      * @param string $msg
      */
     private function error403($msg = 'Forbidden')
