0cfa957245dd846b4ab047b1e420efb51ce19fbb
Attogram Project <attogram@users.noreply.github.com>
2017-12-29 00:06:51 +0100

    default.sources.sql

---
+++ config/default.sources.sql
@@ -0,0 +1,2 @@
+INSERT INTO source ('title', 'host', 'endpoint') VALUES ('commons', 'https://commons.wikimedia.org', '/w/api.php');
+INSERT INTO source ('title', 'host', 'endpoint') VALUES ('enwiki', 'https://en.wikipedia.org', '/w/api.php');
