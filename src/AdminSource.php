06fb2133778fe8e054a4253b228fac52b40ccbb0
Attogram Project <attogram@users.noreply.github.com>
2017-12-30 10:29:12 +0100

    source.delete

--- src/AdminSource.php
+++ src/AdminSource.php
@@ -2,8 +2,6 @@
 
 namespace Attogram\SharedMedia\Gallery;
 
-use Attogram\SharedMedia\Api\Sources;
-use Attogram\SharedMedia\Gallery\Seeder;
 use Attogram\SharedMedia\Orm\Source;
 use Attogram\SharedMedia\Orm\SourceQuery;
 use Propel\Runtime\Map\TableMap;
@@ -18,11 +16,13 @@ class AdminSource
     use TraitView;
 
     private $data = [];
+    private $listUrl = '';
 
     public function __construct($data)
     {
         $this->data = $data;
         $this->accessControl();
+        $this->listUrl = $this->data['uriBase'] . '/admin/source/list/';
     }
 
     public function list()
@@ -43,7 +43,6 @@ class AdminSource
         $title = $this->getPost('t');
         $host = $this->getPost('h');
         $endpoint = $this->getPost('e');
-        $listUrl = $this->data['uriBase'] . '/admin/source/list/';
         if (!$title || !$host || !$endpoint) {
             $this->redirect302($listUrl);
         }
@@ -57,6 +56,60 @@ class AdminSource
         } catch (Throwable $error) {
             $this->error403('ERROR: ' . $error->getMessage());
         }
-        $this->redirect302($listUrl);
+        $this->redirect302($this->listUrl);
+    }
+
+    public function delete()
+    {
+        $sourceId = (int) $this->data['vars'][0];
+        if (!$sourceId || !$this->isNumber($sourceId)) {
+            $this->error404('404 Source ID Not Found');
+        }
+
+        $postSourceId = (int) $this->getPost('sourceId');
+        if (!$postSourceId) {
+            $this->deleteConfirm($sourceId);
+            return;
+        }
+        if (!$postSourceId || !$this->isNumber($postSourceId) || $sourceId !== $postSourceId) {
+            $this->error404('404 Source ID Not Found');
+        }
+        try {
+            $source = SourceQuery::create()->findOneById($sourceId);
+            $source->delete();
+        } catch (Throwable $error) {
+            print $error->getMessage();
+            exit;
+        }
+        $this->redirect302($this->listUrl);
+    }
+
+    /**
+     * @param int $sourceId
+     */
+    private function deleteConfirm($sourceId)
+    {
+        if (!$this->setSource($sourceId)) {
+            $this->error404('404 Source Not Found');
+        }
+        $this->displayView('admin/source.delete');
+    }
+
+    /**
+     * @param int $sourceId
+     * @return bool
+     */
+    private function setSource($sourceId)
+    {
+        try {
+            $source = SourceQuery::create()->findOneById($sourceId);
+            if (!$source instanceof Source) {
+                return false;
+            }
+            $this->data['source'] = $source->toArray(TableMap::TYPE_FIELDNAME);
+            return true;
+        } catch (Throwable $error) {
+            $this->error404('404 Source Query Failed');
+        }
     }
 }
